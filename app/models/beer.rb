fe05e7972c0dddf999be8b1d5086c757fde9580d
Juhana Laurinharju <juhana.laurinharju@gmail.com>
2013-09-03 13:15:42 +0300

    Tehtävä 4: avarage_rating

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,4 +3,12 @@ class Beer < ActiveRecord::Base
 
   belongs_to :brewery
   has_many :ratings
+
+  def avarage_rating
+      return 0 if ratings.empty?
+
+      numeric_ratings = ratings.map &:score
+      sum = numeric_ratings.inject(:+)
+      sum / numeric_ratings.size
+  end
 end
