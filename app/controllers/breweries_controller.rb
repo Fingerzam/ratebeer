487db3f48a6ac396244228b6d2dad2058e16c55b
Juhana Laurinharju <juhana.laurinharju@gmail.com>
2013-09-03 23:44:59 +0300

    Tehtävä 13: kirjautuminen

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,4 +1,5 @@
 class BreweriesController < ApplicationController
+  before_filter :authenticate, only: [:new, :create, :destroy]
   # GET /breweries
   # GET /breweries.json
   def index
@@ -78,6 +79,17 @@ class BreweriesController < ApplicationController
     respond_to do |format|
       format.html { redirect_to breweries_url }
       format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def authenticate
+    admin_accounts = {"admin" => "secret", "pekka" => "beer",
+                      "arto" => "foobar", "matti" => "ittam"}
+
+    authenticate_or_request_with_http_basic do |username, password|
+      admin_accounts.has_key?(username) and admin_accounts[username] == password
     end
   end
 end
