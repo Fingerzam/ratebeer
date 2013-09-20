0daa797e10d4cb0e2660846b7305c395e4b33761
Juhana Laurinharju <juhana.laurinharju@gmail.com>
2013-09-20 18:45:24 +0300

    Refaktoroi testejä

--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -7,33 +7,30 @@ describe "Breweries page" do
     expect(page).to have_content 'number of breweries 0'
   end
 
-  it "lists the existing breweries and their total number" do
-    breweries = ["Koff", "Karjala", "Schlenkerla"]
-    for brewery in breweries
-      FactoryGirl.create(:brewery, name: brewery)
+  describe "when breweries exist" do
+    before :each do
+      @breweries = ["Koff", "Karjala", "Schlenkerla"]
+      year = 1896
+      for brewery in @breweries
+        FactoryGirl.create(:brewery, name: brewery, year: year+= 1)
+      end
+
+      visit breweries_path
     end
 
-    visit breweries_path
+    it "lists the existing breweries and their total number" do
+      expect(page).to have_content "number of breweries #{@breweries.count}"
 
-    expect(page).to have_content "number of breweries #{breweries.count}"
-
-    for brewery in breweries
-      expect(page).to have_content brewery
-    end
-  end
-
-  it "allows user to navigate to page of a Brewery" do
-    breweries = ["Koff, Karjala", "Schelnkerla"]
-    year = 1896
-    for brewery in breweries
-      FactoryGirl.create(:brewery, name: brewery, year: year+= 1)
+      for brewery in @breweries
+        expect(page).to have_content brewery
+      end
     end
 
-    visit breweries_path
+    it "allows user to navigate to page of a Brewery" do
+      click_link "Koff"
 
-    click_link "Koff"
-
-    expect(page).to have_content "Koff"
-    expect(page).to have_content "Established year 1897"
+      expect(page).to have_content "Koff"
+      expect(page).to have_content "Established year 1897"
+    end
   end
 end
