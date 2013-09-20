59efda8f5bd35a883c4ddb5c524b9be82c6b2eeb
Juhana Laurinharju <juhana.laurinharju@gmail.com>
2013-09-20 18:31:58 +0300

    Tehtävä 5: Muokattu panimoiden sivu testien kanssa yhteensopivaksi.

--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -6,4 +6,34 @@ describe "Breweries page" do
     expect(page).to have_content 'Listing breweries'
     expect(page).to have_content 'number of breweries 0'
   end
+
+  it "lists the existing breweries and their total number" do
+    breweries = ["Koff", "Karjala", "Schlenkerla"]
+    for brewery in breweries
+      FactoryGirl.create(:brewery, name: brewery)
+    end
+
+    visit breweries_path
+
+    expect(page).to have_content "number of breweries #{breweries.count}"
+
+    for brewery in breweries
+      expect(page).to have_content brewery
+    end
+  end
+
+  it "allows user to navigate to page of a Brewery" do
+    breweries = ["Koff, Karjala", "Schelnkerla"]
+    year = 1896
+    for brewery in breweries
+      FactoryGirl.create(:brewery, name: brewery, year: year+= 1)
+    end
+
+    visit breweries_path
+
+    click_link "Koff"
+
+    expect(page).to have_content "Koff"
+    expect(page).to have_content "Established year 1897"
+  end
 end
