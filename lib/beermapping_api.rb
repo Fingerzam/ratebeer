a74fa071d7f533b964165f5059447ff3a14f64d8
Juhana Laurinharju <fingerzam@gmail.com>
2013-10-06 22:32:54 +0300

    Viikko 5, tehtävä 4: cache expiration

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,7 +1,18 @@
 class BeermappingAPI
   def self.places_in city
-    url = "http://beermapping.com/webservice/loccity/#{key}/"
+    Place
+    Time
+    city = city.downcase
 
+    Rails.cache.fetch(city, expires_in: 1.hour) do
+      fetch_places_in(city)
+    end
+  end
+
+  private
+
+  def self.fetch_places_in(city)
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
     response = HTTParty .get "#{url}#{city.gsub(' ', '%20')}"
     places = response.parsed_response["bmp_locations"]["location"]
 
