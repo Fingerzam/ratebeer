6e65f3c43fa4397b0d53d9b32b0c754253fbf7f2
Juhana Laurinharju <fingerzam@gmail.com>
2013-10-12 23:20:54 +0300

    V6, T11: desimaalitarkkuus ekskiarvoissa

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def edit_and_destroy_buttons(item)
+    if signed_in?
+      edit = link_to('edit', url_for([:edit, item]), class: "btn")
+      del = link_to('Destroy', item, method: :delete,
+                    data: {confirm: 'Are you sure?'}, class: "btn btn-danger")
+      raw("#{edit} #{del}")
+    end
+  end
+
+  def round(decimal)
+    with_precision = number_with_precision(decimal, precision: 1)
+    raw("#{with_precision}")
+  end
 end
